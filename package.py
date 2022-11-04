6556a6ded617c9ccfcf00a11702a28f336f1e950
Artur Ardai <krkerdo@gmail.com>
2022-11-04 17:09:27 +0300

    ye

---
+++ package.py
@@ -0,0 +1,19 @@
+from datetime import *
+from dateutil.relativedelta import *
+now = datetime.now()
+print(now)
+
+now = now + relativedelta(months=1, weeks=1, hour=10)
+
+print(now)
+
+
+#python -m venv env
+#env/Scripts/activate
+#pip freeze
+#pip install python-dateutil
+#pip freeze
+#deactivate
+#
+#
+#

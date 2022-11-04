3dd506c5760efa3e95e0c3405f837afc5bf79988
Artur Ardai <krkerdo@gmail.com>
2022-11-04 16:25:11 +0300

    what the

---
+++ C3/basic.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+
+
+a=11
+y=a*3.26
+print(str(a) + " parsecs is " + str(y) + " lightyears")
+
+
+import sys
+
+
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+
